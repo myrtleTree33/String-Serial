2497122a2efa889650c1a3763996f5241f55d27f
Joel Tong <me@joeltong.org>
2012-03-27 22:58:13 +0800

    Appended MIT license and relevant information to files

--- StringSerial.h
+++ StringSerial.h
@@ -1,3 +1,28 @@
+/*
+
+**   STRING-SERIAL   **
+** class for Arduino **
+
+Copyright (C) 2012 TONG Haowen Joel
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+	@Name		: TONG Haowen Joel
+	@Organization	: Singapore Academy for Young Engineers and Scientists (SAYES), Science Centre Singapore
+	@Email		: code {at} joeltong {dot} org
+	@Language	: Arduino-C
+	@Descr		: This code simplies Serial communications using the Arduino using the Xbee radio.  Strings are C++ - style strings, and 				  are terminated by a terminator character (default: '%').
+			
+			  Works well with either SoftwareSerial class (Arduino IDE ver 1.0 above) or/and hardware Serial UART. 
+			  
+			  To use, place StringSerial.cpp and StringSerial.h in the directory "<PATH-TO-ARDUINO-IDE-LIBRARY>/StringSerial/".
+
+*/
+
 /* StringSerial.h */
 
 #ifndef StringSerial_h
